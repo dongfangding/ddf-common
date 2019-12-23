6d6521de426cb51ee48f6447c0c8caeda207ccdf
dingdongfang <Yichen&942436>
2019-12-23 18:55:09 +0800

    mqEventListener支持多个实现

---
+++ ddf-common-mq/src/main/java/com/ddf/boot/common/mq/listener/SpringContextRefreshListener.java
@@ -0,0 +1,56 @@
+package com.ddf.boot.common.mq.listener;
+
+import com.ddf.boot.common.mq.config.CustomizeMessagePostProcessor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * $
+ * <p>
+ * <p>
+ * _ooOoo_
+ * o8888888o
+ * 88" . "88
+ * (| -_- |)
+ * O\ = /O
+ * ___/`---'\____
+ * .   ' \\| |// `.
+ * / \\||| : |||// \
+ * / _||||| -:- |||||- \
+ * | | \\\ - /// | |
+ * | \_| ''\---/'' | |
+ * \ .-\__ `-` ___/-. /
+ * ___`. .' /--.--\ `. . __
+ * ."" '< `.___\_<|>_/___.' >'"".
+ * | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * \ \ `-. \_ __\ /__ _/ .-` / /
+ * ======`-.____`-.___\_____/___.-`____.-'======
+ * `=---='
+ * .............................................
+ * 佛曰：bug泛滥，我已瘫痪！
+ *
+ * @author dongfang.ding
+ * @date 2019/12/23 0023 18:15
+ */
+@Component
+@Slf4j
+public class SpringContextRefreshListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private CustomizeMessagePostProcessor customizeMessagePostProcessor;
+
+    /**
+     * Handle an application event.
+     *
+     * @param event the event to respond to
+     */
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        RabbitTemplate bean = event.getApplicationContext().getBean(RabbitTemplate.class);
+        bean.addBeforePublishPostProcessors(customizeMessagePostProcessor);
+    }
+}
