d8d6fbf82cf5e0ed93195d93d11109639bdc3fc5
dongfang.ding <1041765757@qq.com>
2020-06-12 18:55:30 +0800

    增加一个记录布尔返回值的日志拦截工具（布尔类型因为有些接口做了幂等判断只更新一次，所以日志需要获取到最终更新结果，有没有走更新）

---
+++ ddf-common-core/src/main/java/com/ddf/boot/common/logbool/LogBoolReturnResult.java
@@ -0,0 +1,35 @@
+package com.ddf.boot.common.logbool;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>记录布尔返回值的返回数据类，可以消费这个数据做日志</p >
+ *
+ * @author dongfang.ding
+ * @version 1.0
+ * @date 2020/06/12 15:35
+ */
+@Data
+@Accessors(chain = true)
+public class LogBoolReturnResult {
+
+    private BoolReturn boolReturn;
+
+    /**
+     * 日志名称
+     */
+    private String logName;
+
+    /**
+     * 执行类名
+     */
+    private String className;
+
+    /**
+     * 执行方法名
+     */
+    private String methodName;
+
+    private String param;
+}
