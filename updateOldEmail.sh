d53030c811e7713fb743195832a2c40e4cf2d709
dongfangding <1041765757@qq.com>
2020-10-23 22:39:11 +0800

    增加脚本

---
+++ updateOldEmail.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+git filter-branch --env-filter '
+OLD_EMAIL="Yichen&942436"
+CORRECT_NAME="dongfang.ding"
+CORRECT_EMAIL="1041765757@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tag
