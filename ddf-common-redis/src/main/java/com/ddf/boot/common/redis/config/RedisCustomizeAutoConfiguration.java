5335052a59e6a1c2464637adf5b93f77f5b2346a
dongfangding <1041765757@qq.com>
2021-04-25 11:42:20 +0800

    扩展redisson属性

--- ddf-common-redis/src/main/java/com/ddf/boot/common/redis/config/RedisCustomizeAutoConfiguration.java
+++ ddf-common-redis/src/main/java/com/ddf/boot/common/redis/config/RedisCustomizeAutoConfiguration.java
@@ -1,9 +1,17 @@
 package com.ddf.boot.common.redis.config;
 
+import cn.hutool.core.util.StrUtil;
 import com.ddf.boot.common.redis.helper.RedisTemplateHelper;
+import lombok.SneakyThrows;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.Codec;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
+import org.redisson.spring.starter.RedissonAutoConfigurationCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -17,7 +25,11 @@ import org.springframework.data.redis.core.StringRedisTemplate;
  */
 @Configuration
 @AutoConfigureAfter(value = {RedissonAutoConfiguration.class})
-public class RedisCustomizeAutoConfiguration {
+@EnableConfigurationProperties({RedissonCustomizeProperties.class})
+public class RedisCustomizeAutoConfiguration implements RedissonAutoConfigurationCustomizer {
+
+    @Autowired
+    private RedissonCustomizeProperties redissonCustomizeProperties;
 
     /**
      * 注册redis扩展方法类
@@ -29,5 +41,20 @@ public class RedisCustomizeAutoConfiguration {
     public RedisTemplateHelper redisTemplateHelper(StringRedisTemplate stringRedisTemplate,
             RedissonClient redissonClient) {
         return new RedisTemplateHelper(stringRedisTemplate, redissonClient);
+    }
+
+    /**
+     * Customize the RedissonClient configuration.
+     *
+     * @param configuration the {@link Config} to customize
+     */
+    @SneakyThrows
+    @Override
+    public void customize(Config configuration) {
+        if (StrUtil.isNotBlank(redissonCustomizeProperties.getCodec())) {
+            configuration.setCodec((Codec) Class.forName(redissonCustomizeProperties.getCodec()).newInstance());
+        } else {
+            configuration.setCodec(new JsonJacksonCodec());
+        }
     }
 }
