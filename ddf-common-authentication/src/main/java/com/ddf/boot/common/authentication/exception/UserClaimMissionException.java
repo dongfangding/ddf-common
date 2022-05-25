208ba5d23583febaa06985c53eb2d867cd2922b0
dongfangding <1041765757@qq.com>
2022-05-25 18:55:30 +0800

    认证模块调整

---
+++ ddf-common-authentication/src/main/java/com/ddf/boot/common/authentication/exception/UserClaimMissionException.java
@@ -0,0 +1,28 @@
+package com.ddf.boot.common.authentication.exception;
+
+import com.ddf.boot.common.core.exception200.BaseCallbackCode;
+import com.ddf.boot.common.core.exception200.BaseErrorCallbackCode;
+import com.ddf.boot.common.core.exception200.BaseException;
+
+/**
+ * 用户信息丢失异常
+ *
+ * @author dongfang.ding
+ * @date 2019-12-07 16:45
+ */
+public class UserClaimMissionException extends BaseException {
+
+    public UserClaimMissionException(String message) {
+        super(message);
+    }
+
+    /**
+     * 当前异常默认响应状态码
+     *
+     * @return
+     */
+    @Override
+    public BaseCallbackCode defaultCallback() {
+        return BaseErrorCallbackCode.UNAUTHORIZED;
+    }
+}
