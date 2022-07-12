fb55308ccfb98709aa612465e3531facbcf10173
dongfangding <1041765757@qq.com>
2022-07-12 16:41:54 +0800

    升级hutool包

--- ddf-common-core/src/main/java/com/ddf/boot/common/core/helper/SpringContextHolder.java
+++ ddf-common-core/src/main/java/com/ddf/boot/common/core/helper/SpringContextHolder.java
@@ -1,38 +1,31 @@
 package com.ddf.boot.common.core.helper;
 
+import cn.hutool.extra.spring.EnableSpringUtil;
+import cn.hutool.extra.spring.SpringUtil;
 import java.util.Map;
-import lombok.Getter;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Component;
 
 /**
- * 注意，无法在容器初始化过程中工作。因为无法保证顺序， 容器初始化的时候，这个类可能还没初始化，会有空指针异常，除非强制依赖该类。
  *
  *
  * 一个工具类，用于获取{@code Spring}的{@link ApplicationContext}，并直接对外暴露一些常用的的方法，
- * 如果需要这里未提供的方法，可以调用{@link SpringContextHolder#getApplicationContext()}然后再具体操作它的方法
  *
  * @author dongfang.ding on 2018/12/31
  */
-@Component
+@Configuration
+@EnableSpringUtil
 @Order(value = Ordered.HIGHEST_PRECEDENCE + 5)
-public class SpringContextHolder implements ApplicationContextAware {
-
-    @Getter
-    private static ApplicationContext applicationContext;
-
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        SpringContextHolder.applicationContext = applicationContext;
-    }
-
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class SpringContextHolder {
 
     /**
      * 下面的判空就是这个工具类存在的意义， 虽然在spring环境中这里一定不为空，但是我可能有一些工具类，
@@ -46,23 +39,36 @@ public class SpringContextHolder implements ApplicationContextAware {
      * @throws BeansException
      */
     public static <T> T getBeanWithStatic(Class<T> requiredType) throws BeansException {
-        return applicationContext == null ? null : applicationContext.getBean(requiredType);
+        try {
+            return getBeanWithStatic(requiredType);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public static <T> T getBean(Class<T> requiredType) throws BeansException {
-        return applicationContext.getBean(requiredType);
+        return SpringUtil.getBean(requiredType);
     }
 
     public static Object getBean(String name) throws BeansException {
-        return applicationContext.getBean(name);
+        return SpringUtil.getBean(name);
     }
 
     public static <T> T getBean(String name, Class<T> requiredType) throws BeansException {
-        return applicationContext.getBean(name, requiredType);
+        return SpringUtil.getBean(name, requiredType);
     }
 
     public static <T> Map<String, T> getBeansOfType(@Nullable Class<T> type) throws BeansException {
-        return applicationContext.getBeansOfType(type);
+        return SpringUtil.getBeansOfType(type);
+    }
+
+    /**
+     * 获取{@link ApplicationContext}
+     *
+     * @return {@link ApplicationContext}
+     */
+    public static ApplicationContext getApplicationContext() {
+        return SpringUtil.getApplicationContext();
     }
 
     /**
@@ -74,7 +80,9 @@ public class SpringContextHolder implements ApplicationContextAware {
      */
     public static <T> boolean containsBeanType(Class<T> type) {
         try {
-            applicationContext.getBean(type);
+            if (Objects.isNull(SpringUtil.getBean(type))) {
+                return Boolean.FALSE;
+            }
             return Boolean.TRUE;
         } catch (NoSuchBeanDefinitionException e) {
             return Boolean.FALSE;
