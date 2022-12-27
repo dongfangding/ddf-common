f47a43d192faf37a98d15ccaa643ea3f72fbdc53
dongfangding <1041765757@qq.com>
2022-12-27 13:32:32 +0800

    feat:取消RestController默认的通用包装行为

--- ddf-common-core/src/main/java/com/ddf/boot/common/core/controllerwrapper/AbstractCommonResponseBodyAdvice.java
+++ ddf-common-core/src/main/java/com/ddf/boot/common/core/controllerwrapper/AbstractCommonResponseBodyAdvice.java
@@ -4,7 +4,6 @@ import com.ddf.boot.common.api.model.common.response.response.ResponseData;
 import java.lang.reflect.AnnotatedElement;
 import java.util.Arrays;
 import org.springframework.core.MethodParameter;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
@@ -14,41 +13,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 /**
  * 允许在执行一个@ResponseBody 或一个ResponseEntity控制器方法之后但在使用一个主体写入正文之前自定义响应HttpMessageConverter。
- * <p>
  * 如果不想要自己的返回值生效，可以在控制器类上使用@Controller代替@RestController或者{@link CommonResponseBodyAdviceProperties}
- * <p>
- * _ooOoo_
- * o8888888o
- * 88" . "88
- * (| -_- |)
- * O\ = /O
- * ___/`---'\____
- * .   ' \\| |// `.
- * / \\||| : |||// \
- * / _||||| -:- |||||- \
- * | | \\\ - /// | |
- * | \_| ''\---/'' | |
- * \ .-\__ `-` ___/-. /
- * ___`. .' /--.--\ `. . __
- * ."" '< `.___\_<|>_/___.' >'"".
- * | | : `- \`.;`\ _ /`;.`/ - ` : | |
- * \ \ `-. \_ __\ /__ _/ .-` / /
- * ======`-.____`-.___\_____/___.-`____.-'======
- * `=---='
- * .............................................
- * 佛曰：bug泛滥，我已瘫痪！
+ *
+ * 这里提供逻辑，但没有将该类加入到容器中，就是为了让其他服务直接集成该类，然后再添加自己的@RestControllerAdvice(basePackages = {""})
  *
  * @author dongfang.ding
  * @date 2019/6/27 11:15
  */
-@RestControllerAdvice(basePackages = {"com"})
-@Order
-public class CommonResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+public class AbstractCommonResponseBodyAdvice implements ResponseBodyAdvice<Object> {
 
     private static final Class[] ANNOTATIONS = {
             RequestMapping.class, GetMapping.class, PostMapping.class, DeleteMapping.class, PutMapping.class
