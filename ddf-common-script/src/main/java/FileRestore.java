c2d8035615387daabe29c0fc54f804769863bf03
snowball <snowball@emqil.com>
2024-07-09 19:37:09 +0800

    feat: 文件去重

--- ddf-common-script/src/main/java/FileRestore.java
+++ ddf-common-script/src/main/java/FileRestore.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,13 +9,11 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileTime;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Objects;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * <p>description</p >
@@ -25,8 +25,8 @@ import java.util.Objects;
 public class FileRestore {
 
     public static void main(String[] args) {
-        String baseTargetDirectory = "D:/迅雷下载/restore";
-        computerReadAndMoveFileToMonth(new String[] {"D:/迅雷下载/视频"}, baseTargetDirectory);
+        String baseTargetDirectory = "D:/迅雷下载/backup";
+        deleteRepeatFileByMd5(new String[] {"D:/迅雷下载/QQ空间备份_1041765757/Albums"}, baseTargetDirectory);
     }
 
     /**
@@ -75,119 +75,10 @@ public class FileRestore {
         }
     }
 
-
-    /**
-     * 适用于原手机文件，直接读取文件的创建时间，将文件按创建时间的月份和天进行归档整理
-     *
-     * @param directories
-     * @param baseTargetDirectory
-     */
-    public static void computerReadAndMoveFileToDay(String[] directories, String baseTargetDirectory) {
-        String notVidVideoPath = baseTargetDirectory + "/not_vid";
-        for (String directory : directories) {
-            try {
-                Files.walkFileTree(Paths.get(directory), new SimpleFileVisitor<>() {
-                    @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                        // 获取文件创建时间
-                        String fileName = file.getFileName().toString();
-                        if (!fileName.startsWith("VID")) {
-                            // 如果文件名不是以"VID"开头，移动到not_vid目录
-                            Path notVidPath = Paths.get(notVidVideoPath);
-                            if (!Files.exists(notVidPath)) {
-                                Files.createDirectories(notVidPath);
-                            }
-                            Path targetPath = notVidPath.resolve(file.getFileName());
-                            Files.move(file, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                            System.out.println("Moved " + file.getFileName() + " to " + targetPath);
-                        } else {
-                            String month = fileName.substring(3, 9);
-                            String day = fileName.substring(3, 11);
-                            // 创建月目录
-                            Path monthDir = Paths.get(baseTargetDirectory, month);
-                            if (!Files.exists(monthDir)) {
-                                Files.createDirectories(monthDir);
-                            }
-
-                            // 创建日目录
-                            Path dayDir = monthDir.resolve(day);
-                            if (!Files.exists(dayDir)) {
-                                Files.createDirectories(dayDir);
-                            }
-
-                            // 移动文件到目标日期目录
-                            Path targetPath = dayDir.resolve(file.getFileName());
-                            Files.move(file, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                            System.out.println("Moved " + file.getFileName() + " to " + targetPath);
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    /**
-     * 从电脑中读取文件，适用于监控录像，按照文件格式截取创建时间，因录像一天内小文件较多，所以按照整理天汇总
-     *
-     * @param directories
-     * @param baseTargetDirectory
-     */
-    public static void computerMonitorReadAndMoveFileToDay(String[] directories, String baseTargetDirectory) {
-        String notVidVideoPath = baseTargetDirectory + "/not_vid";
-        for (String directory : directories) {
-            try {
-                Files.walkFileTree(Paths.get(directory), new SimpleFileVisitor<>() {
-                    @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                        // 获取文件创建时间
-                        String fileName = file.getFileName().toString();
-                        if (!fileName.startsWith("VID")) {
-                            // 如果文件名不是以"VID"开头，移动到not_vid目录
-                            Path notVidPath = Paths.get(notVidVideoPath);
-                            if (!Files.exists(notVidPath)) {
-                                Files.createDirectories(notVidPath);
-                            }
-                            Path targetPath = notVidPath.resolve(file.getFileName());
-                            Files.move(file, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                            System.out.println("Moved " + file.getFileName() + " to " + targetPath);
-                        } else {
-                            String month = fileName.substring(3, 9);
-                            String day = fileName.substring(3, 11);
-                            // 创建月目录
-                            Path monthDir = Paths.get(baseTargetDirectory, month);
-                            if (!Files.exists(monthDir)) {
-                                Files.createDirectories(monthDir);
-                            }
-
-                            // 创建日目录
-                            Path dayDir = monthDir.resolve(day);
-                            if (!Files.exists(dayDir)) {
-                                Files.createDirectories(dayDir);
-                            }
-
-                            // 移动文件到目标日期目录
-                            Path targetPath = dayDir.resolve(file.getFileName());
-                            Files.move(file, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                            System.out.println("Moved " + file.getFileName() + " to " + targetPath);
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
     /**
      * 监控录像使用的，录像文件夹的特点
      * 1. 一级目录 2024011220，然后下面就全部是这个时间点的文件
-     *
+     * <p>
      * 解决问题， 由于上面的问题，导致文件夹特别多，处理成如下
      * 1. 一级目录到月202401
      * 2. 再创建二级目录到天20240112
@@ -207,8 +98,10 @@ public class FileRestore {
                         String sourceFolderName = folder.getName();
 
                         // 截取一级目录、二级目录、三级目录
-                        String firstLevelDir = sourceFolderName.substring(0, Math.min(sourceFolderName.length(), 6)); // 前6位作为一级目录
-                        String secondLevelDir = sourceFolderName.substring(0, Math.min(sourceFolderName.length(), 8)); // 前8位作为二级目录
+                        String firstLevelDir = sourceFolderName.substring(
+                                0, Math.min(sourceFolderName.length(), 6)); // 前6位作为一级目录
+                        String secondLevelDir = sourceFolderName.substring(
+                                0, Math.min(sourceFolderName.length(), 8)); // 前8位作为二级目录
 
                         // 构建目标路径
                         Path targetPath = Paths.get(targetDirector, firstLevelDir, secondLevelDir, sourceFolderName);
@@ -221,7 +114,7 @@ public class FileRestore {
                         Files.move(sourcePath, targetPath);
 
                         // 删除空源文件夹
-//                        Files.delete(sourcePath);
+                        //                        Files.delete(sourcePath);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -229,6 +122,72 @@ public class FileRestore {
             }
 
             System.out.println("All folders moved successfully.");
+        }
+    }
+
+
+    public static void deleteRepeatFileByMd5(String[] directories, String backupDeleteDirector) {
+        Set<String> md5Set = new HashSet<>();
+        for (String directory : directories) {
+            try {
+                Files.walkFileTree(Paths.get(directory), new SimpleFileVisitor<>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        final File realFile = file.toFile();
+                        final String md5 = getMD5(realFile);
+                        if (!md5Set.contains(md5)) {
+                            md5Set.add(md5);
+                            return FileVisitResult.CONTINUE;
+                        }
+                        Path deleteDir = Paths.get(backupDeleteDirector);
+                        if (!Files.exists(deleteDir)) {
+                            Files.createDirectories(deleteDir);
+                        }
+
+                        Path targetPath = deleteDir.resolve(realFile.getName());
+                        Files.move(realFile.toPath(), targetPath);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    // --------------------- 辅助方法
+
+
+    /**
+     * 获取文件md5
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     */
+    public static String getMD5(File file) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            try (InputStream is = new FileInputStream(file); DigestInputStream dis = new DigestInputStream(is, md)) {
+                byte[] buffer = new byte[8192];
+                while (dis.read(buffer) != -1) {
+                    ; // 读取文件内容以计算哈希值
+                }
+            }
+
+            byte[] md5Bytes = md.digest();
+
+            // 转换为十六进制字符串
+            StringBuilder md5 = new StringBuilder();
+            for (byte b : md5Bytes) {
+                md5.append(String.format("%02x", b));
+            }
+            return md5.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
