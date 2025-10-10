c28a80cd960440001bcafd8b846f79a506371de5
snowball <snowball@email.com>
2025-10-10 16:07:57 +0800

    refactor(redis):优化本地缓存配置并新增key生成器
    - 移除LocalCacheConfiguration中冗余的注释和代码
    - 简化RedisCacheManagerConfiguration中的日志记录方式
    - 新增ApplicationNamedKeyGenerator用于统一key命名规则
    - 调整缓存管理器配置以提高性能和可维护性

---
+++ ddf-common-redis/src/main/java/com/ddf/boot/common/redis/constant/ApplicationNamedKeyGenerator.java
@@ -0,0 +1,58 @@
+package com.ddf.boot.common.redis.constant;
+
+import com.ddf.boot.common.core.helper.SpringContextHolder;
+import com.google.common.base.Joiner;
+import reactor.util.annotation.NonNull;
+
+/**
+ * <p>key generator</p >
+ * 该类提供以模块名命名的风格：applicationName:key
+ *
+ * @author ropbot
+ */
+public class ApplicationNamedKeyGenerator {
+
+    private static final String KEY_SPLIT = ":";
+
+    private static final Joiner JOINER = Joiner.on(KEY_SPLIT);
+
+    private static final String GLOBAL_NAME = "global";
+
+    private static final String APPLICATION_NAME = SpringContextHolder
+            .getApplicationContext().getEnvironment().getProperty("spring.application.name");
+
+    /**
+     * 拼凑key
+     *
+     * @param keys
+     * @return
+     */
+    public static String genKey(@NonNull String... keys) {
+        return genKey(false, keys);
+    }
+
+    /**
+     * 拼凑key
+     *
+     * @param ignoreApplicationName 是否忽略applicationName，若忽略则用'global'代替
+     * @param keys
+     * @return
+     */
+    public static String genKey(boolean ignoreApplicationName, @NonNull String... keys) {
+        String[] params = new String[keys.length + 1];
+        params[0] = ignoreApplicationName ? GLOBAL_NAME : APPLICATION_NAME;
+        System.arraycopy(keys, 0, params, 1, params.length - 1);
+        return JOINER.join(params);
+    }
+
+    /**
+     * 拼凑key
+     *
+     * @param keys
+     * @return
+     */
+    public static String genNormalKey(@NonNull String... keys) {
+        return JOINER.join(keys);
+    }
+
+}
